585b65f5b026ea004752340be3bc8a593bb9585b
khandelwalitm <khandelwalitm@yahoo.co.in>
2026-02-26 21:58:38 +0530

    pushing my js on 2602

---
+++ SampleJScoding.js
@@ -0,0 +1,11 @@
+console.log("Hello World");
+console.log(2+3);
+console.log("2+3");
+console.log("Sample set of statement");
+console.log(5+2-3,"arthimetic operation-");
+console.log(true+false+200);
+//+ symbol is used to performa the arithmetic operation and + used to join the data
+//Print NaN while appending the string with - , *, /
+console.log(9+4-2 + "set of instruction-"+45-5+10);
+console.log(9+4-2 + "set of instruction-"+45+5+10);
+
